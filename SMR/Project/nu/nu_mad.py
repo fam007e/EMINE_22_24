4d13a2e30c1ddefe1fe481e729bca43dba121022
Dark Knightz <faisalmoshiur@gmail.com>
2023-07-16 22:23:15 +0200

    Changes to be committed: 	modified:   SMR/Project/nu/nu_mad.py

--- SMR/Project/nu/nu_mad.py
+++ SMR/Project/nu/nu_mad.py
@@ -1,9 +1,12 @@
 numbers = [0.1, 0.4]
 sum_value = sum(numbers)
+count = len(numbers)
 
-while sum_value < 50:
+while sum_value < 50.0:
     print(numbers[-2], end=" ")
     sum_value += numbers[-2]
     numbers.append(0.5)
+    count += 1
 
 print("\nSum of the generated numbers:", sum_value)
+print("Number of generated numbers:", count)
