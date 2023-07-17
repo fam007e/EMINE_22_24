de699c74741a0579ec1a8a5718397bf5f279aa71
Dark Knightz <faisalmoshiur@gmail.com>
2023-07-17 09:39:30 +0200

    Changes to be committed: 	deleted:    ESADE/NVCP/RadPow.txt 	new file:   SMR/Project/nu/ST.py

---
+++ SMR/Project/nu/ST.py
@@ -0,0 +1,22 @@
+import serpentTools
+import matplotlib.pyplot as plt
+
+# Read the depletion file
+dep = serpentTools.read("LFR_4.unknown_dep.m")
+
+
+'''
+# Access burnup and mass data
+burnup = dep.burnup
+mass = dep.macroscopicData["U235"]
+
+# Plotting
+plt.plot(burnup, mass, marker='o')
+plt.xlabel('Burnup [MWd/kg]')
+plt.ylabel('U235 Mass')
+plt.title('U235 Mass vs Burnup')
+plt.grid(True)
+plt.show()
+'''
+#print(dir(dep))
+
